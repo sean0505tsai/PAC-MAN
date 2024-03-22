71627b075ee33274d19f95eee43481b334b3167f
sean0505tsai <80439835+sean0505tsai@users.noreply.github.com>
2024-03-22 09:35:01 +0800

    Revert "editing map"
    This reverts commit b66b0a1fc74509f778ac3895f64533d87d3dfb4d.

--- Source/Game/gameMap.h
+++ Source/Game/gameMap.h
@@ -5,15 +5,15 @@
 
 namespace game_framework {
 
-	class GameMap : public CMovingBitmap
+	class gameMap : public CMovingBitmap
 	{
 	public:
 		bool isCollision(int x, int y);
 	protected:
 	private:
-		bool isLevelPass;
 		int mapMatrix[30][27];
-		CMovingBitmap point;
-		CMovingBitmap lifeCount[3];				// 顯示生命數
+
 	};
 }
+
+
--- Source/Game/ghost.h
+++ Source/Game/ghost.h
@@ -10,39 +10,9 @@ namespace game_framework {
 		virtual void onMove();
 
 	protected:
-		int speed;					// 移動速度
-		int currentDirection;		// 目前面相方向
-		int currentState;			// 目前狀態
-		enum direction{UP, DOWN, LEFT, RIGHT};
-		enum state{STRONG, WEAK};		// 狀態: 無敵/虛弱
+		int speed;
+		enum direction{ UP, DOWN, LEFT, RIGHT};
 	};
 
-	class Character : public ghost {
-	public:
-		virtual void onMove() override;
-		bool isAlive();				// 是否死亡
-	private:
-		int lifeCount;				// 生命數
-	};
-
-	class RedGhost : public ghost {
-	public:
-		virtual void onMove() override;
-	};
-
-	class OrangeGhost : public ghost {
-	public:
-		virtual void onMove() override;
-	};
-
-	class BlueGhost : public ghost {
-	public:
-		virtual void onMove() override;
-	};
-
-	class PinkGhost : public ghost {
-	public:
-		virtual void onMove() override;
-	};
 }
 
